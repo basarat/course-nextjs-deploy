323a10d9755d89c14160172289eca38452477d6c
basarat <basaratali@gmail.com>
2025-04-02 11:50:13 +1100

    deploy

---
+++ src/app/base-url/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { BASE_URL } from "@/lib/BASE_URL";
+
+export default function Page() {
+  return (
+    <>
+      <h1>BASE_URL</h1>
+      <p>{BASE_URL}</p>
+    </>
+  );
+}
